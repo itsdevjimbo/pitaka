b0b9f76615f9730845ccf38f2fc497847ad3779c
itsdevjimbo <dev.jimbo10@gmail.com>
2026-08-09 21:21:11 +0800

    feat: Refactor seeding logic and add User and Account seeders

--- PitakaApp.Api/Data/DbSeeder.cs
+++ PitakaApp.Api/Data/DbSeeder.cs
@@ -7,14 +7,18 @@ public static class DbSeeder
     public static void Seed(DbContext context)
     {
         AdminSeeder.Seed(context);
+        UserSeeder.Seed(context);
         CategorySeeder.Seed(context);
+        AccountSeeder.Seed(context);
         context.SaveChanges();
     }
 
     public static async Task SeedAsync(DbContext context, CancellationToken cancellationToken)
     {
         AdminSeeder.Seed(context);
+        UserSeeder.Seed(context);
         CategorySeeder.Seed(context);
+        AccountSeeder.Seed(context);
         await context.SaveChangesAsync(cancellationToken);
     }
 }
