772b9f8fbdbdab254f8e223e09e19b18b1e6c03f
itsdevjimbo <dev.jimbo10@gmail.com>
2026-08-18 00:49:22 +0800

    add missing id in goal resource

--- PitakaApp.Api/Resources/GoalResource.cs
+++ PitakaApp.Api/Resources/GoalResource.cs
@@ -3,10 +3,10 @@ using PitakaApp.Api.Models;
 
 namespace PitakaApp.Api.Resources;
 
-public record GoalResource(string Name, decimal TargetAmount, DateOnly? TargetDate, GoalStatus Status)
+public record GoalResource(int Id, string Name, decimal TargetAmount, DateOnly? TargetDate, GoalStatus Status)
 {
     public static GoalResource FromModel(Goal goal) =>
-        new(goal.Name, goal.TargetAmount, goal.TargetDate, goal.Status);
+        new(goal.Id, goal.Name, goal.TargetAmount, goal.TargetDate, goal.Status);
 
     public static List<GoalResource> Collection(IEnumerable<Goal> goals) =>
         goals.Select(FromModel).ToList();
